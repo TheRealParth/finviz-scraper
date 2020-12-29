a4a62a3c4cdcffc5dbe2eb9876177735d46b69b6
JimLynchCodes <mrdotjim@gmail.com>
2020-12-28 20:47:03 -0500

    adds winston logger

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { getTickerList } from './utils/get-ticker-list'
 import { getDataForTickers } from './utils/scrape-data-for-tickers'
 
+import { logger } from './utils/logger'
+
 const main = async () => {
   const tickerList = getTickerList()
 
-  console.log('Input data: ', tickerList)
+  logger.info(`Input data: ${JSON.stringify(tickerList, null, 2)}`)
 
   return getDataForTickers(tickerList)
 }
 
 main().then(data => {
-  console.log('data for tickers: ', data)
+  logger.info(`data for tickers: ${JSON.stringify(data, null, 2)}`)
   process.exit(0)
 })
